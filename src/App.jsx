9d8e42ce7de5eb0a87ac28b76ca3e2c491c500af
Fernanda Barros Linhares <fernanda_linhares@estudante.sesisenai.org.br>
2024-07-11 17:25:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import {Contador} from "./components/contador"
+
+function App() {
+ 
+  
+
+  return (
+    <>
+     <Contador></Contador>
+    </>
+  )
+}
+
+export default App
